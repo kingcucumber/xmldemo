d46998815a4e15d7a3cf928b52dc98c51e939622
kingcucumber <jinhua.wang18@gmail.com>
2012-11-23 15:56:42 +0800

    nothing just for test!

---
+++ src/com/xml/saxDemo/Demo2.java
@@ -0,0 +1,58 @@
+package com.xml.saxDemo;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+public class Demo2 {
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException{
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		
+		SAXParser sp = factory.newSAXParser();
+		
+		XMLReader reader = sp.getXMLReader();
+		
+		reader.setContentHandler(new ListHandler());
+		
+		reader.parse("src/book.xml");
+	}
+}
+
+class TagValueHandler extends DefaultHandler{
+	
+	private String currentTag ; //record the current tag
+
+	public void characters(char[] ch, int start, int length)
+			throws SAXException {
+		// TODO Auto-generated method stub
+		super.characters(ch, start, length);
+		
+		if("зїеп".equals(currentTag)){
+			System.out.println(new String());
+		}
+	}
+
+	public void endElement(String uri, String localName, String qName)
+			throws SAXException {
+		// TODO Auto-generated method stub
+		super.endElement(uri, localName, qName);
+	}
+
+	public void startElement(String uri, String localName, String qName,
+			Attributes attributes) throws SAXException {
+		// TODO Auto-generated method stub
+		super.startElement(uri, localName, qName, attributes);
+		
+		currentTag = qName;
+	}
+	
+}
